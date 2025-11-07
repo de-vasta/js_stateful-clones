a3964fe410bcf726c5ae6dbdc859083455f0705c
Nazar Kundys <nazarrisd@gmail.com>
2025-11-07 19:23:42 +0200

    ✍🏼 `transformStateWithClones` function to handle ➕add, ❌remove, and 🧹clear (based on `actions`) props of the `state` object without reassigning function's params and returning the array of modified object of every action. ➕ simple `switch` `default:`

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,38 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const ADD_ACTION = 'addProperties';
+  const REMOVE_ACTION = 'removeProperties';
+  const CLEAR_ACTION = 'clear';
+
+  // Array to hold clones of `state` after each action
+  const stateClones = [];
+  const prevClone = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ADD_ACTION:
+        Object.assign(prevClone, action.extraData);
+        break;
+
+      case REMOVE_ACTION:
+        for (const key of action.keysToRemove) {
+          delete prevClone[key];
+        }
+        break;
+
+      case CLEAR_ACTION:
+        for (const key in prevClone) {
+          delete prevClone[key];
+        }
+        break;
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+    stateClones.push({ ...prevClone });
+  }
+
+  return stateClones;
 }
 
 module.exports = transformStateWithClones;
